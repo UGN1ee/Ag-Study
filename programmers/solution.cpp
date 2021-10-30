aaa5e81a65a3e7962147933882e1aa663f70f107
Eugene Lee <93449757+UGN1ee@users.noreply.github.com>
2021-10-31 05:04:27 +0900

    완주하지 못한 선수

---
+++ programmers/solution.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	int s = participant.size();
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+	for (int i = 0; i < s-1; i++) {
+		if (participant[i] != completion[i]) {
+			answer = participant[i];
+			break;
+		}
+	}
+	if (answer == "") {
+		answer = participant[s-1];
+	}
+	return answer;
+}
